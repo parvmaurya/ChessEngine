498788ed1e672ff53c95f97d35344fe1d10cb0a9
Parv Maurya <parvmaurya@gmail.com>
2020-01-02 19:29:21 +0530

    insert image to readme

--- board.js
+++ board.js
@@ -21,41 +21,41 @@ class Board{
 
     setupPieces(){
         this.whitePieces.push(new King(4,7,true));
-        // this.whitePieces.push(new Bishop(2,7,true));
-        // this.whitePieces.push(new Bishop(5,7,true));
-        // this.whitePieces.push(new Rook(0,7,true));
-        // this.whitePieces.push(new Rook(7,7,true));
+        this.whitePieces.push(new Bishop(2,7,true));
+        this.whitePieces.push(new Bishop(5,7,true));
+        this.whitePieces.push(new Rook(0,7,true));
+        this.whitePieces.push(new Rook(7,7,true));
         this.whitePieces.push(new Queen(3,7,true));
-        // this.whitePieces.push(new Knight(1,7,true));
-        // this.whitePieces.push(new Knight(6,7,true));
+        this.whitePieces.push(new Knight(1,7,true));
+        this.whitePieces.push(new Knight(6,7,true));
 
-        // this.whitePieces.push(new Pawn(0,6,true));
-        // this.whitePieces.push(new Pawn(1,6,true));
-        // this.whitePieces.push(new Pawn(2,6,true));
-        // this.whitePieces.push(new Pawn(3,6,true));
-        // this.whitePieces.push(new Pawn(4,6,true));
-        // this.whitePieces.push(new Pawn(5,6,true));
-        // this.whitePieces.push(new Pawn(6,6,true));
-        // this.whitePieces.push(new Pawn(7,6,true));
+        this.whitePieces.push(new Pawn(0,6,true));
+        this.whitePieces.push(new Pawn(1,6,true));
+        this.whitePieces.push(new Pawn(2,6,true));
+        this.whitePieces.push(new Pawn(3,6,true));
+        this.whitePieces.push(new Pawn(4,6,true));
+        this.whitePieces.push(new Pawn(5,6,true));
+        this.whitePieces.push(new Pawn(6,6,true));
+        this.whitePieces.push(new Pawn(7,6,true));
 
 
         this.blackPieces.push(new King(4,0,false));
-        //this.blackPieces.push(new Bishop(2,0,false));
-        //this.blackPieces.push(new Bishop(5,0,false));
-        //this.blackPieces.push(new Rook(0,0,false));
-        //this.blackPieces.push(new Rook(7,0,false));
-        //this.blackPieces.push(new Queen(3,0,false));
-        //this.blackPieces.push(new Knight(1,0,false));
-        //this.blackPieces.push(new Knight(6,0,false));
+        this.blackPieces.push(new Bishop(2,0,false));
+        this.blackPieces.push(new Bishop(5,0,false));
+        this.blackPieces.push(new Rook(0,0,false));
+        this.blackPieces.push(new Rook(7,0,false));
+        this.blackPieces.push(new Queen(3,0,false));
+        this.blackPieces.push(new Knight(1,0,false));
+        this.blackPieces.push(new Knight(6,0,false));
 
-        //this.blackPieces.push(new Pawn(0,1,false));
-        //this.blackPieces.push(new Pawn(1,1,false));
-        //this.blackPieces.push(new Pawn(2,1,false));
-        //this.blackPieces.push(new Pawn(3,1,false));
-        //this.blackPieces.push(new Pawn(4,1,false));
-        //this.blackPieces.push(new Pawn(5,1,false));
-        //this.blackPieces.push(new Pawn(6,1,false));
-        //this.blackPieces.push(new Pawn(7,1,false));
+        this.blackPieces.push(new Pawn(0,1,false));
+        this.blackPieces.push(new Pawn(1,1,false));
+        this.blackPieces.push(new Pawn(2,1,false));
+        this.blackPieces.push(new Pawn(3,1,false));
+        this.blackPieces.push(new Pawn(4,1,false));
+        this.blackPieces.push(new Pawn(5,1,false));
+        this.blackPieces.push(new Pawn(6,1,false));
+        this.blackPieces.push(new Pawn(7,1,false));
     }
 
     pieceAt(xpos,ypos){
